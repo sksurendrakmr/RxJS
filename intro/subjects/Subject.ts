118019ea15cb5fba05a6d403729bba0eaf54b616
sksurendrakmr <sksurendrakmr@gmail.com>
2021-09-27 18:47:11 +0530

    Subject inRxJS

---
+++ intro/subjects/Subject.ts
@@ -0,0 +1,14 @@
+import { fromEvent, Subject } from "rxjs";
+
+const emitButton = document.querySelector("button#emit");
+const inputElement: HTMLInputElement = document.querySelector("#value-input");
+const subscribeButton = document.querySelector("button#subscribe");
+
+const value$ = new Subject<string>();
+
+fromEvent(emitButton, "click").subscribe(() => value$.next(inputElement.value));
+
+fromEvent(subscribeButton, "click").subscribe(() => {
+  console.log("New Subscription");
+  value$.subscribe((value) => console.log(value));
+});
